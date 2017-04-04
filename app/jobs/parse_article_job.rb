b53ffba0099a7d140db17c06490e3aa2f7d6223f
Ararat777 <dimension18@mail.ru>
2017-04-05 02:00:15 +0300

    add comments

--- app/jobs/parse_article_job.rb
+++ app/jobs/parse_article_job.rb
@@ -7,10 +7,11 @@ class ParseArticleJob < ApplicationJob
     articles_on_page = 30
     if count.to_i > articles_on_page  
         
-        #Все статьи с первой страницы
+        #Первая страница
         page = parser.get("https://news.ycombinator.com/")
         authors = page.search('.subtext').take(articles_on_page)
         articles = page.search('.athing').take(articles_on_page)
+        #Вторая страница
         page = parser.get("https://news.ycombinator.com/news?p=2")
         authors += page.search('.subtext').take(count.to_i - articles_on_page)
         articles += page.search('.athing').take(count.to_i - articles_on_page)
